d3dba42451980ed75db354bd96092e8c0b5fd2f9
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-04-14 17:06:12 -0300

    feat: validate middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { handleApplicationErrors } from "@/middlewares/error-handling-middleare";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(handleApplicationErrors)
 
 
 export default app;
